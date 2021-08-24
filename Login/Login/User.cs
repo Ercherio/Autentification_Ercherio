6caed02a2f7867c394f4846dc7b6badabf0234b4
Ercherio <85327437+Ercherio@users.noreply.github.com>
2021-08-24 12:08:55 +0700

    UploadFile Project

---
+++ Login/Login/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login
+{
+    class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password {protected get; set; }
+        public string Username { get; set; }
+
+        public User(string firstName, string lastName, string password, string username)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            Username = username;
+        }
+
+        public User()
+        {
+        }
+
+        public string CheckPassword()
+        {
+            return Password;
+        }
+        public string UpdatePassword(string newPassword)
+        {
+            return Password = newPassword;
+        }
+    }
+}
+
